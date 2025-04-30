0d72d0a23a74a8a3274911d5b1d6704b7bf51717
Irvan Castro <castroirvan@gmail.com>
2025-04-30 14:04:20 +0800

    create verifyToken middleware

---
+++ src/midlewares/auth.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
+
+export interface AuthPayload {
+  id: number;
+  username: string;
+  iat: number;
+  exp: number;
+}
+
+export const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    res.status(401).json({ message: 'Unauthorized' });
+    return 
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as AuthPayload;
+    req.user = decoded; // Attach the decoded token to the request object
+    next();
+  } catch (error) {
+    console.error('Invalid or Expired Token:', error);
+    res.status(401).json({ message: 'Forbidden' });
+  }
+};
