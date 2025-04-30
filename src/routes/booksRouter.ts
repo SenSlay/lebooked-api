ecb770ce367edc0235d54a5890498f94cfd8e334
Irvan Castro <castroirvan@gmail.com>
2025-04-30 16:03:45 +0800

    protect post and delete routes

--- src/routes/booksRouter.ts
+++ src/routes/booksRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { createBook, deleteBook, getAllBooks, getBookById } from '../controllers/booksController.js';
+import { isAdmin, verifyToken } from '../midlewares/auth.js';
 
 const booksRouter = Router();
 
 booksRouter.get('/', getAllBooks);
 booksRouter.get('/:id', getBookById);
-booksRouter.post('/', createBook);
-booksRouter.delete('/:id', deleteBook)
+booksRouter.post('/', verifyToken, isAdmin, createBook);
+booksRouter.delete('/:id', verifyToken, isAdmin, deleteBook)
 
 export default booksRouter;
