5f94f2382e2e032c076f90b232f27661483f49bc
Irvan Castro <castroirvan@gmail.com>
2025-05-01 16:43:07 +0800

    refactor: change getAllBooks to getBooks

--- src/routes/booksRouter.ts
+++ src/routes/booksRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { createBook, deleteBook, getAllBooks, getBookById } from '../controllers/booksController.js';
+import { createBook, deleteBook, getBooks, getBookById } from '../controllers/booksController.js';
 import { isAdmin, verifyToken } from '../midlewares/auth.js';
 
 const booksRouter = Router();
 
-booksRouter.get('/', getAllBooks);
+booksRouter.get('/', getBooks);
 booksRouter.get('/:id', getBookById);
 booksRouter.post('/', verifyToken, isAdmin, createBook);
 booksRouter.delete('/:id', verifyToken, isAdmin, deleteBook)
