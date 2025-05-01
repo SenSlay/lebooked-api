605f6df34f134dc2b874386b83dc0fe547798b71
Irvan Castro <castroirvan@gmail.com>
2025-05-01 16:43:44 +0800

    add cors origin and connect app to our routers

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,24 @@ const app = express();
 import cors from 'cors';
 import "dotenv/config";
 import booksRouter from './routes/booksRouter.js';
+import authRouter from './routes/authRouter.js';
+import cartRouter from './routes/cartRouter.js';
+import tagsRouter from './routes/tagsRouter.js';
+import genresRouter from './routes/genresRouter.js';
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN,
+  credentials: true,
+}));;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/books', booksRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/genres', genresRouter);
+app.use('/api/tags', tagsRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
